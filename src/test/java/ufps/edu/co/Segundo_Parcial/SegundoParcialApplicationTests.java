82640d307500434060c41b0826a0e2fb568b0813
WithBaz <jorgesebastianflorezdiaz2004@gmail.com>
2024-11-07 15:18:37 -0500

    first commit

---
+++ src/test/java/ufps/edu/co/Segundo_Parcial/SegundoParcialApplicationTests.java
@@ -0,0 +1,13 @@
+package ufps.edu.co.Segundo_Parcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundoParcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
